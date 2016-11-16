ef8ae9690e1b28a8fc417102a715853635c8c12d
Ahmet Kapusuz <ahmetkapusuz@gmail.com>
2016-11-16 22:49:30 +0300

    Resource file url is changed

--- src/com/ahmetkapusuz/kelimeonerici/KelimeOnerici.java
+++ src/com/ahmetkapusuz/kelimeonerici/KelimeOnerici.java
@@ -1,49 +1,38 @@
 package com.ahmetkapusuz.kelimeonerici;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class KelimeOnerici {
 
 	private ArrayList<String> suggestedWordList;
-	private String trFilePath = "res\\word_list_tr.txt";
-	private String enFilePath = "res\\word_list_en.txt";
+	private String trFilePath = "word_list_tr.txt";
+	private String enFilePath = "word_list_en.txt";
+	private Scanner scannerTr;
+	private Scanner scannerEn;
 
 	public KelimeOnerici() {
 
 	}
 
 	public ArrayList<String> suggestWord(String word) {
-		String suggestedWord;
 
-		File trFile = new File(trFilePath);
-		File enFile = new File(enFilePath);
+		scannerTr = new Scanner(getClass().getResourceAsStream("/" + trFilePath), "UTF-8");
+		scannerEn = new Scanner(getClass().getResourceAsStream("/" + enFilePath));
 
-		Scanner scannerTr;
-		Scanner scannerEn;
-		try {
-			scannerTr = new Scanner(trFile, "UTF-8");
-			scannerEn = new Scanner(enFile);
-
-			while (scannerTr.hasNextLine()) {
-				String trLineFromFile = scannerTr.nextLine();
-				String enLineFromFile = scannerEn.nextLine();
+		while (scannerTr.hasNextLine()) {
+			String trLineFromFile = scannerTr.nextLine();
+			String enLineFromFile = scannerEn.nextLine();
 //				if (enLineFromFile.contains(word)) {
 //					// a match!
 //					System.out.println("En file:  " + word + " -  Tr File: " + trLineFromFile);
 //					break;
 //				}
-				if (enLineFromFile.equals(word)) {
-					// a match!
-					System.out.println("En file:  " + word + " -  Tr File: " + trLineFromFile);
-					break;
-				}
+			if (enLineFromFile.equals(word)) {
+				System.out.println("En file:  " + word + " -  Tr File: " + trLineFromFile);
+				break;
 			}
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 
 		return suggestedWordList;
