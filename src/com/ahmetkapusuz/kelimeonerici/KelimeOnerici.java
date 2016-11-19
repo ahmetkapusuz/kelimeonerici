6ca521c5361e72d3d1da29757523f7dc36969b6b
Ahmet <Ahmet@Ahmet-MacBook-Air.local>
2016-11-19 12:08:08 +0300

    Javadocs are added

--- src/com/ahmetkapusuz/kelimeonerici/KelimeOnerici.java
+++ src/com/ahmetkapusuz/kelimeonerici/KelimeOnerici.java
@@ -5,10 +5,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * Kelime Onerici is created to suggest words which have Turkish Characters 
+ * when get a Turkish word without Turkish characters as an input.
+ * 
+ * @author Ahmet Kapusuz
+ *
+ */
 public class KelimeOnerici {
 
+	// Word list to return
 	private ArrayList<String> suggestedWordList;
+	
+	// Turkish dictionary with Turkish characters
 	private String trFilePath = "word_list_tr.txt";
+	
+	// Turkish dictionary without Turkish characters
 	private String enFilePath = "word_list_en.txt";
 	private Scanner scannerTr;
 	private Scanner scannerEn;
@@ -17,6 +29,12 @@ public class KelimeOnerici {
 		
 	}
 
+	/**
+	 * Suggest words that has a full match
+	 * 
+	 * @param word Word without Turkish characters
+	 * @return List of the words
+	 */
 	public List<String> suggestWords(String word) {
 		suggestedWordList = new ArrayList<>();
 		scannerTr = new Scanner(getClass().getResourceAsStream("/" + trFilePath), "UTF-8");
@@ -38,6 +56,12 @@ public class KelimeOnerici {
 
 	}
 	
+	/**
+	 * Suggest words that starts with the given input word
+	 * 
+	 * @param word Word without Turkish characters
+	 * @return List of the words
+	 */
 	public List<String> suggestWordsStartsWith(String word) {
 		suggestedWordList = new ArrayList<>();
 		scannerTr = new Scanner(getClass().getResourceAsStream("/" + trFilePath), "UTF-8");
@@ -59,6 +83,12 @@ public class KelimeOnerici {
 
 	}
 	
+	/**
+	 * Suggest words that contains the given word
+	 * 
+	 * @param word Word without Turkish characters
+	 * @return List of the words
+	 */
 	public List<String> suggestWordsContains(String word) {
 		suggestedWordList = new ArrayList<>();
 		scannerTr = new Scanner(getClass().getResourceAsStream("/" + trFilePath), "UTF-8");
